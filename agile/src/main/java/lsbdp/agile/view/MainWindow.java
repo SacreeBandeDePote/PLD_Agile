38ed44f4f78607ac7ce36f019eb95d1660653546
P4py <matthieu.valvin@gmail.com>
2017-11-16 11:12:07 +0100

    fxml

--- agile/src/main/java/lsbdp/agile/view/MainWindow.java
+++ agile/src/main/java/lsbdp/agile/view/MainWindow.java
@@ -1,6 +1,7 @@
 package lsbdp.agile.view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
@@ -9,6 +10,9 @@ public class MainWindow extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
+		
+		//Scene scene = FXMLLoader.load("MainWindow.fxml");
+		
 		Scene scene = new Scene(new Label("Main Window"));
 		primaryStage.setScene(scene);
 		
