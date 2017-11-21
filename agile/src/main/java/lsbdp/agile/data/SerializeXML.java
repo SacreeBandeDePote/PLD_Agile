660aedab2875c3a61dfd3dfe7b9a7becec230c1c
Corentin Giraud <corentin.giraud@insa-lyon.fr>
2017-11-21 18:19:21 +0100

    serializeXML class functional and ready to be reviewed/merged. AlgoTest sucks #Turpin :)

--- agile/src/main/java/lsbdp/agile/data/SerializeXML.java
+++ agile/src/main/java/lsbdp/agile/data/SerializeXML.java
@@ -25,99 +25,93 @@ import lsbdp.agile.model.Street;
 import lsbdp.agile.model.StreetMap;
 
 public class SerializeXML {
-	
-	   public static void main(String[] args) throws ParseException {
-		   SerializeMapXML(new File("Data/fichiersXML/planLyonPetit.xml"));
-	   }
-	   public static StreetMap SerializeMapXML(File fileXML) throws ParseException {
-		      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		      StreetMap streetMap = new StreetMap();
-		      try {
-		         DocumentBuilder builder = factory.newDocumentBuilder();
-		         Document xml = builder.parse(fileXML);
-		         Element root = xml.getDocumentElement();		       
-		         final NodeList racineNoeuds = root.getChildNodes();
-		         final int nbRacineNoeuds = racineNoeuds.getLength();
 
-	        	 
-		         for (int i = 0; i<nbRacineNoeuds; i++) {
-	        	    if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE){			        
-	        	        final Element element = (Element) racineNoeuds.item(i);
-	        	        if(element.getNodeName() == "noeud")
-	        	        {			        	      
-	        	        	Float id = Float.parseFloat(element.getAttribute("id"));
-	        	        	int x = Integer.parseInt(element.getAttribute("x"));
-	        	        	int y = Integer.parseInt(element.getAttribute("y"));
-	        	        	streetMap.put(id,new Intersection(id,x,y));
-	        	        }     	      	        
-	        	        if(element.getNodeName() == "troncon")
-	        	        {
-	        	        	Float end = Float.parseFloat(element.getAttribute("destination"));
-	        	        	Float origin = Float.parseFloat(element.getAttribute("origine"));
-	        	        	float length = Float.parseFloat(element.getAttribute("longueur"));
-	        	        	String name = element.getAttribute("nomRue");
-	        	        	Intersection originIntersection = streetMap.getIntersection(origin);
-	        	        	Intersection endIntersection = streetMap.getIntersection(end);	        	      
-	        	        	originIntersection.addStreet(new Street (length, name, endIntersection));
-	        	        	
-	        	      	}				
-	        	    }
-	        	}		             
-		      } catch (ParserConfigurationException e) {
-		    	  e.printStackTrace();
-		      } catch (SAXException e) {
-		          e.printStackTrace();
-		      } catch (IOException e) {
-		          e.printStackTrace();
-		      }
-			return streetMap;
-	   }
-	   
-	   
-	   public static DeliveriesRequest SerializeDeliveryXML(File fileXML) throws ParseException {
-		 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		 DeliveriesRequest demandeDeLivraisons = null;
-		 try {
-			 DocumentBuilder builder = factory.newDocumentBuilder();
-	         Document xml = builder.parse(fileXML);
-	         Element root = xml.getDocumentElement();
-	         
-	         final NodeList racineNoeuds = root.getChildNodes();
-	         final int nbRacineNoeuds = racineNoeuds.getLength();
-	         DateFormat sdf = new SimpleDateFormat("hh:mm:ss");
-        	 Date startingTime = null;
-        	 Intersection warehouse = null;
-        	 ArrayList <Delivery> deliveryList = new ArrayList<Delivery>();
-	         for (int i = 0; i<nbRacineNoeuds; i++) {
-        	    if(racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE){			        
-        	        final Element element = (Element) racineNoeuds.item(i);
-        	        if(element.getNodeName() == "entrepot")
-        	        {			        	      
-        	        	startingTime = (Date) sdf.parse((element.getAttribute("heureDepart")));		        	        	
-        	        	warehouse = new Intersection (Float.parseFloat(element.getAttribute("adresse")),0,0);			        	    
-        	        }     	      	        
-        	        if(element.getNodeName() == "livraison")
-        	        {
-        	        	Date timespanStart = new Date(0);
-        	        	Date timespanEnd = new Date(0);
-        	        	if(element.getAttribute("debutPlage").length()!=0) timespanStart = (Date) sdf.parse(element.getAttribute("debutPlage"));		        	        	
-        	        	if(element.getAttribute("finPlage").length()!=0) timespanEnd = (Date) sdf.parse(element.getAttribute("finPlage"));		        	        	
-        	        	int duration = Integer.parseInt(element.getAttribute("duree"));
-        	        	Intersection location = new Intersection(Float.parseFloat(element.getAttribute("adresse")),0,0);
-        	        	deliveryList.add(new Delivery(duration,timespanStart,timespanEnd, location));
-	        		}				
-        	    }
-        	}
-	         demandeDeLivraisons = new DeliveriesRequest(startingTime, warehouse, deliveryList);	
-	         
-		 } catch (ParserConfigurationException e) {
-	    	  e.printStackTrace();
-	     } catch (SAXException e) {
-	          e.printStackTrace();
-	     } catch (IOException e) {
-	          e.printStackTrace();
-	     }
-		return demandeDeLivraisons;     
-	   }
-	   
+	public StreetMap serializeMapXML(File fileXML) throws ParseException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		StreetMap streetMap = new StreetMap();
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document xml = builder.parse(fileXML);
+			Element root = xml.getDocumentElement();
+			final NodeList racineNoeuds = root.getChildNodes();
+			final int nbRacineNoeuds = racineNoeuds.getLength();
+
+			for (int i = 0; i < nbRacineNoeuds; i++) {
+				if (racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE) {
+					final Element element = (Element) racineNoeuds.item(i);
+					if (element.getNodeName() == "noeud") {
+						Float id = Float.parseFloat(element.getAttribute("id"));
+						int x = Integer.parseInt(element.getAttribute("x"));
+						int y = Integer.parseInt(element.getAttribute("y"));
+						streetMap.put(id, new Intersection(id, x, y));
+					}
+					if (element.getNodeName() == "troncon") {
+						Float end = Float.parseFloat(element.getAttribute("destination"));
+						Float origin = Float.parseFloat(element.getAttribute("origine"));
+						float length = Float.parseFloat(element.getAttribute("longueur"));
+						String name = element.getAttribute("nomRue");
+						Intersection originIntersection = streetMap.get(origin);
+						Intersection endIntersection = streetMap.get(end);
+						originIntersection.addStreet(new Street(length, name, endIntersection));
+
+					}
+				}
+			}
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return streetMap;
+	}
+
+	public DeliveriesRequest serializeDeliveryXML(File fileXML) throws ParseException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DeliveriesRequest deliveriesRequest = null;
+		try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document xml = builder.parse(fileXML);
+			Element root = xml.getDocumentElement();
+
+			final NodeList racineNoeuds = root.getChildNodes();
+			final int nbRacineNoeuds = racineNoeuds.getLength();
+			DateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+			Date startingTime = null;
+			Intersection warehouse = null;
+			ArrayList<Delivery> deliveryList = new ArrayList<Delivery>();
+			for (int i = 0; i < nbRacineNoeuds; i++) {
+				if (racineNoeuds.item(i).getNodeType() == Node.ELEMENT_NODE) {
+					final Element element = (Element) racineNoeuds.item(i);
+					if (element.getNodeName() == "entrepot") {
+						startingTime = (Date) sdf.parse((element.getAttribute("heureDepart")));
+						warehouse = new Intersection(Float.parseFloat(element.getAttribute("adresse")), 0, 0);
+					}
+					if (element.getNodeName() == "livraison") {
+						Date timespanStart = new Date(0);
+						Date timespanEnd = new Date(0);
+						if (element.getAttribute("debutPlage").length() != 0)
+							timespanStart = (Date) sdf.parse(element.getAttribute("debutPlage"));
+						if (element.getAttribute("finPlage").length() != 0)
+							timespanEnd = (Date) sdf.parse(element.getAttribute("finPlage"));
+						int duration = Integer.parseInt(element.getAttribute("duree"));
+						Intersection location = new Intersection(Float.parseFloat(element.getAttribute("adresse")), 0,
+								0);
+						deliveryList.add(new Delivery(duration, timespanStart, timespanEnd, location));
+					}
+				}
+			}
+			deliveriesRequest = new DeliveriesRequest(startingTime, warehouse, deliveryList);
+
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return deliveriesRequest;
+	}
+
 }
