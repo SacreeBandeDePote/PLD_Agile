daabf088abb54889d8cba7c1fe8d4dc485547b10
lturpin <laurent.turpin@insa-lyon.fr>
2017-11-21 10:49:03 +0100

    Refactoring

--- agile/src/main/java/lsbdp/agile/model/Intersection.java
+++ agile/src/main/java/lsbdp/agile/model/Intersection.java
@@ -1,49 +1,67 @@
 package lsbdp.agile.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Intersection {
 
 	private int id;
 	private int x;
 	private int y;
-	private ArrayList<Street> neighboors = new ArrayList<Street>();
-	
+	private List<Street> streets = new ArrayList<Street>();
+
 	public Intersection(int id, int x, int y) {
 		this.id = id;
 		this.x = x;
 		this.y = y;
-		
+
 	}
-	
-	public ArrayList<Street> getNeighboors() {
-		return neighboors;
+
+	public List<Street> getStreets() {
+		return streets;
 	}
 
 	public void addStreet(Street street) {
-		this.neighboors.add(street);
+		this.streets.add(street);
 	}
 
 	@Override
 	public String toString() {
 		return "Noeud [id=" + id + ", x=" + x + ", y=" + y + "]";
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getX() {
 		return x;
 	}
+
 	public void setX(int x) {
 		this.x = x;
 	}
+
 	public int getY() {
 		return y;
 	}
+
 	public void setY(int y) {
 		this.y = y;
-	}	
+	}
+	
+	public List<Intersection> getNeighboors() {
+		List<Intersection> neighboors = new ArrayList<Intersection>();
+		
+		for(Street street : this.streets) {
+			neighboors.add(street.getEnd());
+		}
+		
+		return neighboors;
+	}
+
 }
