07cffa68f0fcf487046b1d5bfc59a64e3073d5bb
lturpin <laurent.turpin@insa-lyon.fr>
2017-11-21 18:01:18 +0100

    The Dijkstra aglorithm is finished, i have to test it

--- agile/src/main/java/lsbdp/agile/model/StreetMap.java
+++ agile/src/main/java/lsbdp/agile/model/StreetMap.java
@@ -1,21 +1,7 @@
 package lsbdp.agile.model;
 
 import java.util.HashMap;
-import java.util.Map;
 
-public class StreetMap {
+public class StreetMap extends HashMap<Integer, Intersection> {
 
-	private Map<Integer, Intersection> intersections = new HashMap<Integer, Intersection>();
-
-	public Map<Integer, Intersection> getMapIntersections() {
-		return intersections;
-	}
-	
-	public Intersection getIntersection(int id) {
-		return intersections.get(id);
-	}
-
-	public void addIntersection(Intersection intersection) {
-		this.intersections.put(intersection.getId(), intersection);
-	}
 }
