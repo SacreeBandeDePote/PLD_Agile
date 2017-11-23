5efaa1b21ecbe36d4c3fa4aa1848ce50725e67ae
Augustin <augustingaillot@gmail.com>
2017-11-23 17:22:19 +0100

    Basic controller framework

--- agile/src/main/java/lsbdp/agile/controller/CommandDelete.java
+++ agile/src/main/java/lsbdp/agile/controller/CommandDelete.java
@@ -8,8 +8,12 @@ public class CommandDelete extends Command {
 	public CommandDelete(Delivery d) {
 		this.delivery = d;
 	}
+	
+	@Override
 	public void doCommand() {
 	}
+	
+	@Override
 	public void undoCommand() {
 	}
 
--- agile/src/main/java/lsbdp/agile/controller/Controller.java
+++ agile/src/main/java/lsbdp/agile/controller/Controller.java
@@ -1,22 +1,30 @@
 package lsbdp.agile.controller;
 
 import java.io.File;
+import java.text.ParseException;
 
+import lsbdp.agile.data.SerializeXML;
+import lsbdp.agile.model.DeliveriesRequest;
 import lsbdp.agile.model.DeliverySchedule;
 import lsbdp.agile.model.StreetMap;
 
 public class Controller {
 	private CommandList cmdList;
+	private SerializeXML serializer;
 	public Controller() {
 	}
-	public void addMap(File XML, StreetMap map) {
-		
+	public void addMap(File XML, StreetMap map) throws ParseException {
+		map = serializer.serializeMapXML(XML);
 	}
-	public void loadDeliveryRequest(String filename, DeliverySchedule schedule) {
+	public void loadDeliveryRequest(File XML, DeliverySchedule schedule) throws ParseException {
+		DeliveriesRequest dr = serializer.serializeDeliveryXML(XML);
+		//schedule = algo.createDeliverySchedule(dr);
 	}
 	public void undo() {
+		cmdList.undo();
 	}
 	public void redo() {
+		cmdList.redo();
 	}
 	
 }
