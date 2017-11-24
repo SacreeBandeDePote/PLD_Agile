35a4551412a1336214d089ce57f3f896cedb9b37
lturpin <laurent.turpin@insa-lyon.fr>
2017-11-24 10:10:15 +0100

    stupidTSP done

--- agile/src/main/java/lsbdp/agile/model/DeliverySchedule.java
+++ agile/src/main/java/lsbdp/agile/model/DeliverySchedule.java
@@ -4,18 +4,5 @@ import java.util.List;
 
 import javafx.util.Pair;
 
-public class DeliverySchedule {
-	private List<Pair<Route, Delivery>> schedule = new ArrayList<Pair<Route,Delivery>>();
-
-	public List<Pair<Route, Delivery>> getSchedule() {
-		return schedule;
-	}
-
-	public void setSchedule(List<Pair<Route, Delivery>> schedule) {
-		this.schedule = schedule;
-	}
-
-	public DeliverySchedule(List<Pair<Route, Delivery>> schedule) {
-		this.schedule = schedule;
-	} 
+public class DeliverySchedule extends ArrayList<Pair<Route, Delivery>>{
 }
