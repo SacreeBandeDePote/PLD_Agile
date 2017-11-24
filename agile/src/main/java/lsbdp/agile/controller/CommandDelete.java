1e8f39a0ff8ea34f496faa3991edb2e977acd420
Ros <rosbenjamindu01@gmail.com>
2017-11-24 10:26:31 +0100

    do delete

--- agile/src/main/java/lsbdp/agile/controller/CommandDelete.java
+++ agile/src/main/java/lsbdp/agile/controller/CommandDelete.java
@@ -1,20 +1,37 @@
 package lsbdp.agile.controller;
 
+import javafx.util.Pair;
 import lsbdp.agile.model.Delivery;
 import lsbdp.agile.model.DeliverySchedule;
+import lsbdp.agile.model.Route;
 
 public class CommandDelete extends Command {
 	private DeliverySchedule schedule;
-	private Delivery delivery;
-	
-	public CommandDelete(Delivery d, DeliverySchedule s) {
+	private Pair<Route, Delivery> element;
+	private int index;
+	//private Pair<Route, Delivery> nextElement;
+	public CommandDelete(DeliverySchedule s, Pair<Route, Delivery> p) {
 		this.schedule = s;
-		this.delivery = d;
+		this.element = p;
+		
+		
 	}
 	
 	@Override
 	public boolean doCommand() {
+		//Enleve un Delivery d'une DeliverySchedule, prendre ses voisins et calculer leur plus court chemin, et changer la route
 		//algo.deleteDelivery(d);
+		 /*
+		index = schedule.indexOf(element);
+		Pair < Route, Delivery > newElement;
+		
+		startDelivery = schedule.get(index-1).getRight();
+		endDelivery = schedule.get(index+1).getRight();
+		newRoute = algo.compute ( startDelivery , endDelivery );
+		
+		schedule.get(index+1).set(Pair<newRoute, endDelivery>);
+		schedule.remove(index);
+		*/
 		return true;
 	}
 	
