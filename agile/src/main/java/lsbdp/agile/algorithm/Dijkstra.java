476ccf7ff67b777566abef6a26c9495208eb2cb6
lturpin <laurent.turpin@insa-lyon.fr>
2017-11-28 11:34:49 +0100

    start big TSP

--- agile/src/main/java/lsbdp/agile/algorithm/Dijkstra.java
+++ agile/src/main/java/lsbdp/agile/algorithm/Dijkstra.java
@@ -6,82 +6,82 @@ import lsbdp.agile.model.*;
 import java.util.*;
 
 public class Dijkstra {
-    private static final long UNDEFINED = -1l;
+	private static final long UNDEFINED = -1l;
 
-    public static Route performDijkstra(StreetMap map, Intersection start, Intersection end) {
-        Route route = new Route(start);
+	public static Route performDijkstra(StreetMap map, Intersection start, Intersection end) {
+		Route route = new Route(start);
 
-        Map<Long, Float> distances = new HashMap<>();
-        Map<Long, Long> previous = new HashMap<>();
-        Comparator<Intersection> comparator = new RouteComparator(distances);
-        List<Intersection> nonView = new ArrayList<>();
+		Map<Long, Float> distances = new HashMap<>();
+		Map<Long, Long> previous = new HashMap<>();
+		Comparator<Intersection> comparator = new RouteComparator(distances);
+		List<Intersection> nonView = new ArrayList<>();
 
-        //init the non view list
-        for (Intersection node : map.values()) {
-            if (node.getId() == start.getId())
-                distances.put(node.getId(), 0.f);
-            else
-                distances.put(node.getId(), Float.MAX_VALUE); //infinity
+		//init the non view list
+		for (Intersection node : map.values()) {
+			if (node.getId() == start.getId())
+				distances.put(node.getId(), 0.f);
+			else
+				distances.put(node.getId(), Float.MAX_VALUE); //infinity
 
-            previous.put(node.getId(), UNDEFINED); //undefine
-            nonView.add(node);
-        }
+			previous.put(node.getId(), UNDEFINED); //undefine
+			nonView.add(node);
+		}
 
-        //We know that a treated node has its shortest route
-        //then we stop as soon as we go to the end node
-        while (!nonView.get(0).equals(end)) {
-            Intersection current =  nonView.get(0);
-            if (current.equals(end))
-                break;
-            for (Intersection neighbor : current.getNeighbors()) {
-                float dist = distances.get(current.getId()) + current.distTo(neighbor);
-                if (dist < distances.get(neighbor.getId())) {
-                    distances.put(neighbor.getId(), dist);
-                    previous.put(neighbor.getId(), current.getId());
-                }
-            }
-            nonView.remove(0);
-            nonView.sort(comparator);
-        }
+		//We know that a treated node has its shortest route
+		//then we stop as soon as we go to the end node
+		while (!nonView.get(0).equals(end)) {
+			Intersection current = nonView.get(0);
+			if (current.equals(end))
+				break;
+			for (Intersection neighbor : current.getNeighbors()) {
+				float dist = distances.get(current.getId()) + current.distTo(neighbor);
+				if (dist < distances.get(neighbor.getId())) {
+					distances.put(neighbor.getId(), dist);
+					previous.put(neighbor.getId(), current.getId());
+				}
+			}
+			nonView.remove(0);
+			nonView.sort(comparator);
+		}
 
-        long routeId = end.getId();
-        while (routeId != start.getId()) {
-            route.addStreetToTop(map.get(previous.get(routeId)).getStreetTo(map.get(routeId)));
-            routeId = previous.get(routeId);
-        }
-        return route;
-    }
+		long routeId = end.getId();
+		while (routeId != start.getId()) {
+			route.addStreetToTop(map.get(previous.get(routeId)).getStreetTo(map.get(routeId)));
+			routeId = previous.get(routeId);
+		}
+		return route;
+	}
 
-    public static Route[][] createTSPGraph(StreetMap map, Intersection warehouse, List<Delivery> deliveries) {
-        Route[][] graphTSP = new Route[deliveries.size()+1][deliveries.size()+1];
+	public static Route[][] createTSPGraph(StreetMap map, Intersection warehouse, List<Delivery> deliveries) {
+		Route[][] graphTSP = new Route[deliveries.size() + 1][deliveries.size() + 1];
 
-        for(int i=0 ; i<graphTSP.length-1 ; i++) {
-            for(int j=0 ; j<graphTSP.length-1 ; j++) {
-                if (i!=j)
-                    graphTSP[i][j] = Dijkstra.performDijkstra(map, deliveries.get(i).getLocation(), deliveries.get(j).getLocation());
-            }
-            graphTSP[i][graphTSP.length-1] = Dijkstra.performDijkstra(map, deliveries.get(i).getLocation(), warehouse);
-        }
+		for (int i = 0; i < graphTSP.length - 1; i++) {
+			for (int j = 0; j < graphTSP.length - 1; j++) {
+				if (i != j)
+					graphTSP[i][j] = Dijkstra.performDijkstra(map, deliveries.get(i).getLocation(), deliveries.get(j).getLocation());
+			}
+			graphTSP[i][graphTSP.length - 1] = Dijkstra.performDijkstra(map, deliveries.get(i).getLocation(), warehouse);
+		}
 
-        for (int j=0 ; j<graphTSP.length-1 ; j++) {
-            graphTSP[graphTSP.length-1][j] = Dijkstra.performDijkstra(map, warehouse, deliveries.get(j).getLocation());
-        }
+		for (int j = 0; j < graphTSP.length - 1; j++) {
+			graphTSP[graphTSP.length - 1][j] = Dijkstra.performDijkstra(map, warehouse, deliveries.get(j).getLocation());
+		}
 
-        return graphTSP;
-    }
+		return graphTSP;
+	}
 
 
 }
 
 class RouteComparator implements Comparator<Intersection> {
 
-    private Map<Long, Float> distances;
+	private Map<Long, Float> distances;
 
-    RouteComparator(Map<Long, Float> distances) {
-        this.distances = distances;
-    }
+	RouteComparator(Map<Long, Float> distances) {
+		this.distances = distances;
+	}
 
-    public int compare(Intersection o1, Intersection o2) {
-        return (int) (distances.get(o1.getId()) - distances.get(o2.getId())); //increasing order
-    }
+	public int compare(Intersection o1, Intersection o2) {
+		return (int) (distances.get(o1.getId()) - distances.get(o2.getId())); //increasing order
+	}
 }
---
+++ agile/src/main/java/lsbdp/agile/algorithm/TemplateTSP.java
@@ -0,0 +1,22 @@
+package lsbdp.agile.algorithm;
+
+import lsbdp.agile.model.Delivery;
+import lsbdp.agile.model.DeliverySchedule;
+import lsbdp.agile.model.Intersection;
+import lsbdp.agile.model.StreetMap;
+
+import java.util.List;
+
+public class TemplateTSP implements TSP{
+
+
+
+	@Override
+	public void findSolution(DeliverySchedule schedule, StreetMap map, Intersection warehouse, List<Delivery> deliveries) {
+
+	}
+
+	private void adaptModel(DeliverySchedule schedule, StreetMap map, Intersection warehouse, List<Delivery> deliveries) {
+
+	}
+}
