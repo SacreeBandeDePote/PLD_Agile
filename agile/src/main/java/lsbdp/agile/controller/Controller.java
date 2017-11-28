019a4a49115aa423ace1912c3c7295d4fd4cc0e3
Ros <rosbenjamindu01@gmail.com>
2017-11-28 10:40:59 +0100

    Controller_Algo

--- agile/src/main/java/lsbdp/agile/controller/Controller.java
+++ agile/src/main/java/lsbdp/agile/controller/Controller.java
@@ -2,6 +2,7 @@ package lsbdp.agile.controller;
 
 import java.io.File;
 import java.text.ParseException;
+import java.util.List;
 
 import lsbdp.agile.algorithm.Dijkstra;
 import lsbdp.agile.algorithm.GloutonTSP;
@@ -16,42 +17,39 @@ import lsbdp.agile.model.StreetMap;
 public class Controller {
 	private CommandList cmdList;
 	private static StreetMap map;
-	private GloutonTSP algo; 
+	private static GloutonTSP algo; 
 	
 	// Mettre ses méthodes en static
 	public Controller() {
 		this.cmdList = new CommandList();
+		this.algo = new GloutonTSP();
 	}
 	public StreetMap addMap(File XML) throws ParseException {
 		map = SerializeXML.serializeMapXML(XML);
 		return map;
+		// AJOUTER LA PARTIE DRAW MAP  
 	}
 	public static StreetMap getMap() {
 		return map;
 	}
-	public DeliveriesRequest addDeliveriesRequest(File XML) throws ParseException {
+	public static DeliveriesRequest addDeliveriesRequest(File XML) throws ParseException {
 		//return serializer.serializeDeliveryXML(XML);
 		return null;
 	}
-	public Route calculateRoute(Delivery start, Delivery end, StreetMap map) {
+	public static Route calculateRoute(Delivery start, Delivery end) {
 		// Mettre en static et modifier la méthode
 		//Dijkstra dj = new Dijkstra(map);
 		//return dj.performDijkstra(start.getLocation(), end.getLocation());
 		return null;
 	}
-	public DeliverySchedule loadDeliveryRequest(File XML, StreetMap map) throws ParseException {
+	public static DeliverySchedule loadDeliveryRequest(File XML) throws ParseException {
 		DeliveriesRequest dr = SerializeXML.serializeDeliveryXML(XML, map);
 		//schedule = algo.createDeliverySchedule(dr);
 		return null;
 	}
 	
-	public DeliverySchedule findDeliverySchedule() {
-		
-		 DeliverySchedule schedule = new DeliverySchedule();
-		 
-	    // tsp.findSolution(schedule, graphTSP, deliveries);
-	     return schedule;
-		
+	public static void findDeliverySchedule(DeliverySchedule schedule, Intersection warehouse, List<Delivery> deliveries ) {
+		 algo.findSolution(schedule, map, warehouse, deliveries); 	
 	}
 	
 	public void undo() {
