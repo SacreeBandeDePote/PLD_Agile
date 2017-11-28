0633f71332d15d6aca2e56b2cfc9edf0f1142b70
lturpin <laurent.turpin@insa-lyon.fr>
2017-11-28 15:17:04 +0100

    rebase with master

--- agile/src/main/java/lsbdp/agile/algorithm/TemplateTSP.java
+++ agile/src/main/java/lsbdp/agile/algorithm/TemplateTSP.java
@@ -1,9 +1,7 @@
 package lsbdp.agile.algorithm;
 
-import lsbdp.agile.model.Delivery;
-import lsbdp.agile.model.DeliverySchedule;
-import lsbdp.agile.model.Intersection;
-import lsbdp.agile.model.StreetMap;
+import javafx.util.Pair;
+import lsbdp.agile.model.*;
 
 import java.util.List;
 
@@ -16,7 +14,35 @@ public class TemplateTSP implements TSP{
 
 	}
 
-	private void adaptModel(DeliverySchedule schedule, StreetMap map, Intersection warehouse, List<Delivery> deliveries) {
+	private void adaptModel(DeliverySchedule schedule, StreetMap map, Intersection warehouse, DeliveriesRequest req) {
+		List<Delivery> deliveries = req.getDeliveryList();
+		Route[][] graphTSP = Dijkstra.createTSPGraph(map, warehouse, deliveries);
 
+		float[][] timeCost = new float[deliveries.size()+1][deliveries.size()+1];
+		for (int i = 0; i < timeCost.length ; i++) {
+			for (int j = 0; j < timeCost.length ; j++) {
+				if(i == j) {
+					timeCost[i][j] = 0f;
+				}
+				else {
+					timeCost[i][j] = graphTSP[i][j].getTotalTime();
+				}
+			}
+		}
+
+		float[] duration = new float[deliveries.size()+1];
+		for (int i = 0; i < duration.length-1 ; i++) {
+			duration[i] = deliveries.get(i).getDuration();
+		}
+		duration[duration.length-1] = 0f;
+
+	}
+
+	private Delivery getDel(List<Delivery> deliveries, Intersection target) {
+		for (Delivery del : deliveries) {
+			if (del.getLocation().equals(target))
+				return del;
+		}
+		return null;
 	}
 }
