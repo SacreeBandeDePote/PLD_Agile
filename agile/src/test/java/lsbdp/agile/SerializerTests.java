6cc096decc9ccc2646489f5ddcd9fc71c50c8ec9
Piet <vincent.piet@nordnet.fr>
2017-12-04 17:41:01 +0100

    Modif Serializer + testsSerializer Simple OK

--- agile/src/test/java/lsbdp/agile/SerializerTests.java
+++ agile/src/test/java/lsbdp/agile/SerializerTests.java
@@ -1,48 +1,43 @@
 package lsbdp.agile;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import lsbdp.agile.data.SerializerXML;
+import lsbdp.agile.model.DeliveriesRequest;
 import lsbdp.agile.model.Intersection;
-import lsbdp.agile.model.Street;
 import lsbdp.agile.model.StreetMap;
 
 public class SerializerTests {
 	static StreetMap map;
-
-	static Intersection a;
-	static Intersection b;
-	static Intersection c;
-	static Intersection d;
-	static Street aToB;
-	static Street aToC;
-	static Street bToD;
-	static Street cToD;
+	static File mapFile; 
+	static File deliveryFile; 
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		a = new Intersection(0, 0, 0);
-		b = new Intersection(1, 5, 0);
-		c = new Intersection(2, 0, 5);
-		d = new Intersection(3, 5, 5);
-		aToB = new Street(50.f, "aToB", b);
-		aToC = new Street(25.f, "aToC", c);
-		bToD = new Street(10.f, "bToD", d);
-		cToD = new Street(10.f, "cToD", d);
-		
-		a.addStreet(aToB);
-		a.addStreet(aToC);
-		b.addStreet(bToD);
-		c.addStreet(cToD);
-
-		map = new StreetMap();
-
-		map.put(0l, a);
-		map.put(1l, b);
-		map.put(2l, c);
-		map.put(3l, d);
+		mapFile = new File("./Data/fichiersXML/planLyonPetit.xml");		
+		deliveryFile = new File("./Data/fichiersXML/DLPetit5.xml");
+		map = SerializerXML.deserializeMapXML(mapFile);
 	}
 	
 	@Test
+	public void testDeserializeMapXML (){
+		map = SerializerXML.deserializeMapXML(mapFile);
+		Intersection intersection = map.get((long)0);
+		assertEquals(0, intersection.getId());
+		assertEquals(27866, intersection.getX());
+		assertEquals(42785, intersection.getY());	
+	}	
 	
+	@Test
+	public void testDeserializeDeliveryXML (){
+		DeliveriesRequest delivery = SerializerXML.deserializeDeliveryXML(deliveryFile,map);
+		assertEquals(88, delivery.getWarehouse().getId());
+		//assertEquals("Thu Jan 01 08:00:00 CET 1970", delivery.getStartingTime());
+	}	
+
 }
