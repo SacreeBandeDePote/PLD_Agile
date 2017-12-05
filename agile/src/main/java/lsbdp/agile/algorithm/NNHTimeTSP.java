b2863e690f9e184bff69123bcce2013d89242382
lturpin <laurent.turpin@insa-lyon.fr>
2017-12-05 08:51:38 +0100

    prepare rebase

--- agile/src/main/java/lsbdp/agile/algorithm/NNHTimeTSP.java
+++ agile/src/main/java/lsbdp/agile/algorithm/NNHTimeTSP.java
@@ -6,12 +6,27 @@ import java.util.ArrayList;
 
 public class NNHTimeTSP extends ShortestLessCostTSP{
 
+	@Override
+	protected float bound(int crtNode, ArrayList<Integer> nonView, float[][] timeCost, float[] duration, Pair<Float, Float>[] timeWindows) {
+		ArrayList<Integer> toVisit = new ArrayList<>(nonView);
+		int nextIndex;
+		int index = crtNode;
+		float sum = 0.f;
+		for (int i = 0; i < nonView.size(); i++) {
+			nextIndex = getShortestRoute(timeCost[index], toVisit);
+			sum += timeCost[index][nextIndex];
+			index = nextIndex;
+		}
+		//ajout du chemin le plus court vers le warehouse
+		sum += timeCost[index][timeCost.length - 1];
+
+		return sum;
+	}
+
 	public boolean checkTimePossible(int crtNode, ArrayList<Integer> nonView, Pair<Float, Float>[] timeWindows) {
 		float startTime = timeWindows[crtNode].getKey();
-		System.out.println("STARTTIME :" + startTime);
 
 		for (Integer s : nonView) {
-			System.out.println("TIME :" + timeWindows[s].getValue());
 			if (timeWindows[s].getValue() < startTime) {
 				return false;
 			}
