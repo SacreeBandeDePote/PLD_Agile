b0c725f3396159771c69845e5a2aa4f0527a3e26
lturpin <laurent.turpin@insa-lyon.fr>
2017-12-06 23:36:03 +0100

    Add import Hbox

--- agile/src/main/java/lsbdp/agile/view/EventHandlers.java
+++ agile/src/main/java/lsbdp/agile/view/EventHandlers.java
@@ -9,7 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
